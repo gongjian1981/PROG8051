2002423ae034c112cd20b5746619a9f6a0aadfff
Jian Gong <gongjian1981@gmail.com>
2025-02-09 18:24:55 -0500

    feat: Set up the Task1 project and implement some basic validation functions.  - create a solution for Assignment 2  - create a project for Task 1  - write basic code for Task 1

---
+++ Assignment2/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> customerInfoList = new List<string>();
+        while (true)
+        {
+            Console.WriteLine("is there a new customer?(y for yes, n for no)");
+            string hasNewCustomer = Console.ReadLine() ?? "";
+            while (hasNewCustomer != "y" && hasNewCustomer != "n")
+            {
+                Console.WriteLine("input error, please type 'y' for yes, 'n' for no");
+                hasNewCustomer = Console.ReadLine() ?? "";
+            }
+            if (hasNewCustomer == "n")
+            {
+                break;
+            }
+            Console.WriteLine("select the palace need to be wired?h for house, b for barn, or g for garages:");
+            string palace = Console.ReadLine() ?? "";
+            while (palace != "h" && palace != "b" && palace != "g")
+            {
+                Console.WriteLine("input error, please type 'h' for house, 'b' for barn, or 'g' for garages");
+                palace = Console.ReadLine() ?? "";
+            }
+            customerInfoList.Add(hasNewCustomer);
+        }
+        if (customerInfoList.Count == 0)
+        {
+            Console.WriteLine("no new customer");
+        }
+        else
+        {
+            foreach (string customerInfo in customerInfoList)
+            {
+                Console.WriteLine(customerInfo);
+            }
+        }
+    }
+}
