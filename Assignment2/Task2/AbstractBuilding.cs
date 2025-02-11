e9ab3e2870ce22cd803d251e581b3796b55fb4a6
Jian Gong <gongjian1981@gmail.com>
2025-02-11 17:03:11 -0500

    feat: Set up the Task2 project and implement some basic functions.  - create a project for Task 2  - create an inteface 'IBuilding.cs' for requirement  - create an abstract class called 'AbstractBuilding.cs' for requirement

---
+++ Assignment2/Task2/AbstractBuilding.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal abstract class AbstractBuilding : IBuilding
+    {
+        private int buildingSize;
+        private int lightBulbNumber;
+        private int outletNumber;
+        private string creditCardNumber;
+        private readonly List<string> commonTaskList = ["Create wiring schemas", "Purchase necessary parts"];
+        public int BuildingSize { get; set; }
+        public int LightBulbNumber { get; set; }
+        public int OutletNumber { get; set; }
+        public string CreditCardNumber { get; set; }
+
+        public List<string> GetCommonTaskList()
+        {
+            return this.commonTaskList;
+        }
+
+        public abstract List<string> GetCustomizedTaskList();
+
+        public List<string> GetAllOperations()
+        {
+            List<string> operations = new List<string>();
+            if (GetAllOperations() != null)
+            {
+                operations.AddRange(GetAllOperations());
+            }
+            if (GetCustomizedTaskList() != null)
+            {
+                operations.AddRange(GetCustomizedTaskList());
+            }
+            return operations;
+        }
+    }
+}
---
+++ Assignment2/Task2/IBuilding.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2
+{
+    /// <summary>
+    /// Class <c>IBuilding</c> is an interface that holds the structure of a building need to be wired
+    /// </summary>
+    internal interface IBuilding
+    {
+        int BuildingSize { get; set; }
+        int LightBulbNumber { get; set; }
+        int OutletNumber { get; set; }
+        string CreditCardNumber { get; set; }
+
+        List<string> GetCommonTaskList()
+        List<string> GetCustomizedTaskList();
+        List<string> GetAllOperations();
+    }
+}
